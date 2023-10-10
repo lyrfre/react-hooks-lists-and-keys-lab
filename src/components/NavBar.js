d15aabdb86176459d9940711b7f51aa8582155f1
Lyra Freund <lyrafreund@gmail.com>
2023-10-10 12:06:49 -0600

    completed lab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,47 @@
 import React from "react";
 
-function NavBar() {
-  const links = ["home", "about", "projects"];
+// function NavBar(props) {
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+//   return <nav>
+//     {
+//     // <a key="home" href="#home">home</a>;
+//     // <a key="about" href="#about">about</a>
+//     <a key={props.link} href={"#" + props.link}>{props.link}</a>
+
+//     }
+//     </nav>;
+// }
+
+// function linkList () {
+//   const links = ["home", "about", "projects"];
+
+//   const linkElements = links.map((link) =>{
+//     return <NavBar key={link} link={link} />
+//   })
+// }
+
+function NavBar(){
+  const links= ["home", "about", "projects"]
+
+  const linkElements = links.map((link)=>{
+    return <a key={link} href={"#"+{link}}>{link}</a>
+  })
+
+  return <nav>{linkElements}</nav>
 }
+
+// function NavBar() {
+//   const links = ["home", "about", "projects"];
+
+//   const anchors = links.map((link) => (
+//     <a key={link} href={"#" + link}>
+//       {link}
+//     </a>
+//   ));
+//   return <nav>{anchors}</nav>;
+// }
+
+
+
 
 export default NavBar;
